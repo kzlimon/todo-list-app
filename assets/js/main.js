069e88a51ace3b1842678f5e716abaad1cf03e39
Kamruzzaman Limon <mdkzlimon@gmail.com>
2019-04-15 00:53:03 +0600

    code refractored, finalized commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,9 @@
-// check off specific todos by clicking - logic code
-// $("li").on("click", function () {
-//     //if color is gray
-//     if ($(this).css("color") === "rgb(128, 128, 128)") {
-//         //turn it black
-//         $(this).css({
-//             color: "black",
-//             textDecoration: "none"
-//         });
-//     }
-//     //else
-//     else {
-//         //turn it gray
-//         $(this).css({
-//             color: "gray",
-//             textDecoration: "line-through"
-//         });
-//     }
-// });
-
-// check off specific todos by clicking
+// Checked off specific todos by clicking - means completed
 $("ul").on("click", "li", function () {
     $(this).toggleClass("completed");
 });
 
-// click on X to delete a todo
+// Click on Trash button to delete a todo
 $("ul").on("click", "span", function (event) {
     $(this).parent().fadeOut(500, function () {
         $(this).remove();
@@ -31,7 +11,7 @@ $("ul").on("click", "span", function (event) {
     event.stopPropagation();
 });
 
-// creating todos by clicking on enter key
+// Creating todos by clicking on Enter key
 $("input[type='text']").keypress(function (event) {
     if (event.which === 13) {
         // new todo text from input
@@ -40,12 +20,10 @@ $("input[type='text']").keypress(function (event) {
         $(this).val("");
         //create a new li and add to ul
         $("ul").prepend("<li><span><i class='far fa-trash-alt'></i></span> " + todoText + "</li>");
-
-
     }
 });
 
-//Add button toggle
+// Add edit icon toggle - hiding and showing input
 $("#edit-icon").on("click", function () {
     $("input[type='text']").fadeToggle();
 });
